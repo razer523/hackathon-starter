dcb59c7de3cff2d2a3f771837128d4befec01bbd
Raymond Albright <razer523@hotmmail.com>
2020-12-03 14:29:31 -0800

    updated ip

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.205.154.98',
+      host: '18.209.59.240',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
